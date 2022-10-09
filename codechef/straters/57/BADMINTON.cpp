c292261143745dc1dd7aa33935ee85e6c1d77113
Naman Shah <91177774+Naman409@users.noreply.github.com>
2022-10-09 12:33:11 +0530

    Solution BADMINTON.cpp

---
+++ codechef/straters/57/BADMINTON.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test;
+	cin>>test;
+	while(test--){
+	    int serve;
+	    cin>>serve;
+	   cout<<1+serve/2<<endl;
+	}
+	return 0;
+}
